d6a6476749b46d9d5b755200aed7da47ae426cb1
Thiago Leite <leite.tjs@gmail.com>
2023-08-11 17:47:13 -0300

    feat: global error handler

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -11,7 +11,7 @@ export const register = async (
   const registerBodySchema = z.object({
     name: z.string(),
     email: z.string(),
-    password: z.string(),
+    password: z.string().min(6),
   })
 
   const { name, email, password } = registerBodySchema.parse(request.body)
