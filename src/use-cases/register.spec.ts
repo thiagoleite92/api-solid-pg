a41e74aff9247d7d5729b05b70ef3566bbf3e285
Thiago Leite <leite.tjs@gmail.com>
2023-08-12 19:20:55 -0300

    test: first suit of tests

---
+++ src/use-cases/register.spec.ts
@@ -0,0 +1,61 @@
+import { expect, it, describe } from 'vitest'
+import { RegisterUserCase } from './register'
+import { compare } from 'bcryptjs'
+import { InMemoryUsersRepository } from '../repositories/in-memory/in-memory-users-repository'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+
+describe('Use case -> Register', () => {
+  it('should hash user password upon registration', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUserCase(usersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'jdoe@example.com',
+      password: '123456',
+    })
+
+    const isPasswordHashedCorrectly = await compare(
+      '123456',
+      user.password_hash,
+    )
+
+    expect(isPasswordHashedCorrectly).toBe(true)
+  })
+
+  it('should not be able to register with same email twice', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUserCase(usersRepository)
+
+    const email = 'jdoe@example.com'
+
+    await registerUseCase.execute({
+      name: 'John Doe',
+      email,
+      password: '123456',
+    })
+
+    await expect(() =>
+      registerUseCase.execute({
+        name: 'John Doe',
+        email,
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
+  })
+
+  it('should be able to register a user', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUserCase(usersRepository)
+
+    const email = 'jdoe@example.com'
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email,
+      password: '123456',
+    })
+
+    expect(user).toBeDefined()
+  })
+})
