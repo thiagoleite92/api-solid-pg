aa464fc0fc9459ae8253561d4d78f6d9d3ee88ee
Thiago Leite <leite.tjs@gmail.com>
2023-08-14 18:27:20 -0300

    feat: fetch user checkins history use case and tests

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -2,5 +2,6 @@ import { Prisma, CheckIn } from '@prisma/client'
 
 export interface CheckInsRepository {
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
+  findManyByUserId(userId: string, page: number): Promise<Array<CheckIn>>
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
 }
---
+++ src/use-cases/tests/fetch-user-check-ins-history.spec.ts
@@ -0,0 +1,67 @@
+import { expect, it, describe, beforeEach } from 'vitest'
+import { InMemoryCheckInsRepository } from '../../repositories/in-memory/in-memory-check-ins-repository'
+import { FetchUserCheckInsHistoryUseCase } from '../fetch-user-check-ins-history'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let sut: FetchUserCheckInsHistoryUseCase
+
+describe('Use case -> Fetch User Check Ins History', () => {
+  beforeEach(async () => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new FetchUserCheckInsHistoryUseCase(checkInsRepository)
+
+    // await gymsRepository.create({
+    //   description: '',
+    //   title: 'Academia JavaScript',
+    //   latitude: -8.048116,
+    //   longitude: -34.920402,
+    //   id: 'gym-id',
+    //   phone: '',
+    // })
+  })
+
+  it('should be able to fetch user check-in history', async () => {
+    await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    await checkInsRepository.create({
+      gym_id: 'gym-02',
+      user_id: 'user-01',
+    })
+
+    const { checkIns } = await sut.execute({
+      userId: 'user-01',
+      page: 1,
+    })
+
+    expect(checkIns).toBeDefined()
+    expect(checkIns).toHaveLength(2)
+    expect(checkIns).toEqual([
+      expect.objectContaining({ gym_id: 'gym-01' }),
+      expect.objectContaining({ gym_id: 'gym-02' }),
+    ])
+  })
+
+  it('should be able to fetch paginated check-in history', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await checkInsRepository.create({
+        gym_id: `gym-${i}`,
+        user_id: 'user-01',
+      })
+    }
+
+    const { checkIns } = await sut.execute({
+      userId: 'user-01',
+      page: 2,
+    })
+
+    expect(checkIns).toBeDefined()
+    expect(checkIns).toHaveLength(2)
+    expect(checkIns).toEqual([
+      expect.objectContaining({ gym_id: 'gym-21' }),
+      expect.objectContaining({ gym_id: 'gym-22' }),
+    ])
+  })
+})
